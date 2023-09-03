62e42b8d27ed919df90fc4e9c64a1bfa61e45cb0
Athulu <nikodem1703@gmail.com>
2023-09-03 22:29:07 +0200

    Moduły (z Routingiem) oraz Angular Material

--- rejestr-klientow/src/app/app.module.ts
+++ rejestr-klientow/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './modules/core/core.module';
+import { HomeModule } from './modules/home/home.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ClientsModule } from './modules/clients/clients.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    HomeModule,
+    AuthModule,
+    ClientsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

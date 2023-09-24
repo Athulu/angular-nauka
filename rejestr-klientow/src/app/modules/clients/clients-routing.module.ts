bd3684ad02fa3d38bf81a48c765dc4cc31e20063
Athulu <nikodem1703@gmail.com>
2023-09-24 23:57:41 +0200

    guardy

--- rejestr-klientow/src/app/modules/clients/clients-routing.module.ts
+++ rejestr-klientow/src/app/modules/clients/clients-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientsComponent } from './clients.component';
 import { ClientComponent } from './components/client/client.component';
 import { ClientFormComponent } from './components/client-form/client-form.component';
+import { authGuardActivate } from '../core/guards/auth-activate.function.guard';
 
 const routes: Routes = [
-  { path: '', component: ClientsComponent },
+  { path: '', component: ClientsComponent, canActivate: [authGuardActivate] },
   { path: 'dodaj', component: ClientFormComponent },
   { path: ':id', component: ClientComponent },
 ];
